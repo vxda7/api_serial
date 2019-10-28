daf6000bce5defef8b5a66ccbe6575de6d9a45c9
vxda77 <vxda77@gmail.com>
2019-10-28 10:49:12 +0900

    model 생성 완료

--- musics/models.py
+++ musics/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.TextField()
+
+
+class Music(models.Model):
+    aritst = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.TextField()
+
+
+class Comment(models.Model):
+    music = models.ForeignKey(Music, on_delete=models.CASCADE)
+    content = models.TextField()
+    
